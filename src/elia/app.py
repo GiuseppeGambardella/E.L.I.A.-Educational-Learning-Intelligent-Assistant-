8e259d04f1e1925de91c9f8ff48532670b52b0fb
Giuseppe Gambardella <gagiuseppe29@gmail.com>
2025-08-10 15:03:11 +0200

    aboliti warning e aggiustati import

--- src/elia/app.py
+++ src/elia/app.py
@@ -1,5 +1,14 @@
-from server import create_app
+import os
+import warnings
+
+# Disabilita warnings prima di qualsiasi import
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+os.environ['PYTHONWARNINGS'] = 'ignore'
+warnings.filterwarnings("ignore")
+
+
+from elia.server import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=app.config["PORT"], debug=app.config["DEBUG"])
+    app.run(host="127.0.0.1", port=app.config["PORT"], debug=app.config["DEBUG"])
