a40979a2bfd0bd3e59e32fd4164f7729df2ca286
Giuseppe Gambardella <gagiuseppe29@gmail.com>
2025-08-12 12:57:17 +0200

    aggiunti Api url e key

--- src/elia/config.py
+++ src/elia/config.py
@@ -12,7 +12,9 @@ class Config:
     PICOVOICE_WORD = os.getenv("PICOVOICE_WORD")
     PICOVOICE_PARAMS = os.getenv("PICOVOICE_PARAMS")
     AUDIO_DEVICE_INDEX = int(os.getenv("AUDIO_DEVICE_INDEX", 0))
-    ENDPOINT_TRANSCRIBE = os.getenv("ENDPOINT_TRANSCRIBE", "http://localhost:5000/transcribe")
+    ENDPOINT_TRANSCRIBE = os.getenv("ENDPOINT_TRANSCRIBE", "http://localhost:5000/ask")
     WHISPER_MODEL = os.getenv("FWHISPER_MODEL", "small")  # Modello ASR Faster-Whisper
     ASR_CONF_THRESHOLD = float(os.getenv("ASR_CONF_THRESHOLD", 0.60))
     ASR_MIN_WORDS = int(os.getenv("ASR_MIN_WORDS", 3))
+    GEMMA_API_URL = os.getenv("GEMMA_API_URL")
+    GEMMA_API_KEY = os.getenv("GEMMA_API_KEY")
